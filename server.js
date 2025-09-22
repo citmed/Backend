fa2f1f658b5a011eaca65bca09011d0243579a5c
Kevin Rivas <kevinr060799@gmail.com>
2025-09-22 10:57:03 -0500

    Se le pone texto al back

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 
 // Ruta no encontrada
 app.use((req, res, next) => {
+  res.send ('BACKEND CITAMED')
     res.status(404).json({ msg: 'Ruta no encontrada' });
 });
 
